16305e9b1482bed3f9b8fa0576f88ff2b0fe42f7
André Luiz <andreluiz.torrens@gmail.com>
2016-03-18 15:46:49 -0300

    Adicionando todas as páginas ao projeto.

---
+++ grupotorrens/src/Template/ProjetoGrupoTorrens/footer.php
@@ -0,0 +1,104 @@
+<!-- Start Footer Section -->
+<section id="footer-section" class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="section-heading-2">
+                    <h3 class="section-title">
+                        <span>ENDEREÇO</span>
+                    </h3>
+                </div>
+
+                <div class="footer-address">
+                    <ul>
+                        <li class="footer-contact"><i class="fa fa-home"></i>Rua: Acadêmico Inacio Nas. Opazo, nº226</li>
+                        <li class="footer-contact"><i class="fa fa-envelope"></i><a href="#">contato@mrm.com</a></li>
+                        <li class="footer-contact"><i class="fa fa-whatsapp"></i>(13) 9 9146-0869</li>
+                        <li class="footer-contact"><i class="fa fa-facebook"></i><a href="#" target="_blank">Facebok/MRM</a></li>
+                    </ul>
+                </div>
+            </div><!--/.col-md-3 -->
+
+
+            <div class="col-md-3 espacamentoMenuFooter">
+                <div class="section-heading-2">
+                    <h3 class="section-title">
+                        <span>MENU</span>
+                    </h3>
+                </div>
+
+                <div class="footer-address">
+                    <ul>
+                        <li><a href="#" class="active">Home</a></li>
+                        <li><a href="quemSomos.php">Quem Somos</a></li>
+                        <li><a href="servicos.php">Serviços</a></li>
+                        <li><a href="vagas.php">Vagas</a></li>
+                        <li><a href="contato.php">Contato</a></li>
+                    </ul>
+                </div>
+            </div><!--/.col-md-3 -->
+
+            <div class="col-md-3">
+                <div class="section-heading-2">
+                    <h3 class="section-title">
+                        <span>Ligamos para você</span>
+                    </h3>
+                </div>
+                <div class="subscription">
+                    <p>Se preferir, deixe-nos seu telefone que entraremos em contato.</p>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Digite seu nome" id="name" required data-validation-required-message="Please enter your name.">
+                        <input type="text" class="form-control" placeholder="Digite seu telefone" id="tel" required data-validation-required-message="Please enter your tel.">
+                        <input type="submit" class="btn btn-primary" value="Enviar">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="fb-page" data-href="https://www.facebook.com/IBMBrasil" data-tabs="timeline" data-width="392.5" data-height="250" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                            <span style="vertical-align: top;width: 261px;height: 300px;overflow: hidden;">
+                                <iframe name="f3fec856102a64c" width="1000px" height="250px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:page Facebook Social Plugin" src="http://www.facebook.com/v2.5/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D42%23cb%3Dfe6d01c08977b8%26domain%3D%26origin%3Dfile%253A%252F%252F%252Ff4126a41a66294%26relation%3Dparent.parent&amp;container_width=263&amp;height=250&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FIBMBrasil&amp;locale=pt_BR&amp;sdk=joey&amp;show_facepile=true&amp;small_header=true&amp;tabs=timeline" style="border: none;visibility: visible;width: 261px;height: 300px;"></iframe>
+                            </span>
+                </div>
+            </div><!--/.col-md-3 -->
+        </div><!--/.row -->
+    </div><!-- /.container -->
+</section>
+<!-- End Footer Section -->
+
+
+<!-- Start CCopyright Section -->
+<div id="copyright-section" class="copyright-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copyright">
+                    Copyright © 2016. Todos os direitos reservados. Developed by <a href="https://br.linkedin.com/in/andreluizdossantos">Andre Luiz</a>
+                </div>
+            </div>
+        </div><!--/.row -->
+    </div><!-- /.container -->
+</div>
+<!-- End CCopyright Section -->
+
+
+
+<!-- Sulfur JS File -->
+<script src="asset/js/jquery-2.1.3.min.js"></script>
+<script src="asset/js/jquery-migrate-1.2.1.min.js"></script>
+<script src="asset/bootstrap/js/bootstrap.min.js"></script>
+<script src="asset/js/owl.carousel.min.js"></script>
+<script src="asset/js/jquery.appear.js"></script>
+<script src="asset/js/jquery.fitvids.js"></script>
+<script src="asset/js/jquery.nicescroll.min.js"></script>
+<script src="asset/js/lightbox.min.js"></script>
+<script src="asset/js/count-to.js"></script>
+<script src="asset/js/styleswitcher.js"></script>
+
+<script src="asset/js/map.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script src="asset/js/script.js"></script>
+
+
+</body>
+</html>
---
+++ grupotorrens/src/Template/ProjetoGrupoTorrens/topo.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<!--[if IE 8 ]><html class="ie ie8" lang="pt-br"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><html lang="pt-br" class="no-js"> <![endif]-->
+<html lang="pt-br">
+
+<head>
+
+    <!-- Basic -->
+    <title>Grupo Torrens | Home</title>
+
+    <!-- Define Charset -->
+    <meta charset="UTF-8">
+
+    <!-- Responsive Metatag -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Page Description and Author -->
+    <meta name="description" content="Sulfur - Responsive HTML5 Template">
+    <meta name="author" content="Shahriyar Ahmed">
+
+    <!-- Bootstrap CSS  -->
+    <link rel="stylesheet" href="asset/bootstrap/css/bootstrap.min.css" type="text/css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="asset/font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="asset/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="asset/css/owl.theme.css" type="text/css">
+    <link rel="stylesheet" href="asset/css/owl.transitions.css" type="text/css">
+
+    <!-- Css3 Transitions Styles  -->
+    <link rel="stylesheet" type="text/css" href="asset/css/animate.css">
+
+    <!-- Lightbox CSS -->
+    <link rel="stylesheet" type="text/css" href="asset/css/lightbox.css">
+
+    <!-- Sulfur CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="asset/css/style.css">
+
+    <!-- Responsive CSS Style -->
+    <link rel="stylesheet" type="text/css" href="asset/css/responsive.css">
+
+
+    <script src="asset/js/modernizrr.js"></script>
+
+
+</head>
+
+<body>
+<!--Page Plugin Facebook -->
+<div id="fb-root"></div>
+<script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<header class="clearfix">
+
+    <!-- Start Top Bar -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="top-bar">
+                    <div class="row">
+
+                        <div class="col-md-6 hidden-xs">
+                            <!-- Start Contact Info -->
+                            <ul class="contact-details">
+                                <li><a href="#"><i class="fa fa-whatsapp"></i> +13 9 9146-0869</a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-envelope-o"></i> contato@mrm.com</a>
+                                </li>
+                            </ul>
+                            <!-- End Contact Info -->
+                        </div><!-- .col-md-6 -->
+
+                        <div class="col-md-6">
+                            <!-- Start Social Links -->
+                            <ul class="social-list">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-skype"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                </li>
+                            </ul>
+                            <!-- End Social Links -->
+                        </div><!-- .col-md-6 -->
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div><!-- .row -->
+    </div><!-- .container -->
+    <!-- End Top Bar -->
+
+
+
+</header>
